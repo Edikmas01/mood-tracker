1026a1ea6a888377115af2280e42c14fac7ed409
Edikmas01 <edikmaslovskiy@gmail.com>
2024-10-21 15:25:37 +0200

    Added animation for selecting emotions in the MoodTracker component.

--- src/components/MoodTracker/MoodTracker.jsx
+++ src/components/MoodTracker/MoodTracker.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 
 export const MoodTracker = () => {
   const [emojis, setEmojis] = useState([]);
+  const [selectedEmoji, setSelectedEmoji] = useState(null);
 
   useEffect(() => {
     const fetchEmojis = async () => {
@@ -18,14 +19,73 @@ export const MoodTracker = () => {
     fetchEmojis();
   }, []);
 
+  const handleEmojiClick = (emoji, emojiElement) => {
+    setSelectedEmoji(emoji);
+    triggerEmojiConfetti(emoji.img, emojiElement);
+  };
+
+  const triggerEmojiConfetti = (emojiImg, emojiElement) => {
+    const confettiContainer = document.querySelector(".confetti-container");
+
+    const rect = emojiElement.getBoundingClientRect();
+    const offsetX = rect.left + rect.width / 2;
+    const offsetY = rect.top + rect.height / 2;
+
+    for (let i = 0; i < 4; i++) {
+      const confetti = document.createElement("img");
+      confetti.src = emojiImg;
+      confetti.className = "confetti-emoji";
+
+      //v1
+      /*confetti.style.left = `${Math.random() * 100}%`;
+      confetti.style.animationDelay = `${Math.random() * 2}s`;
+      */
+
+      //v2
+      confetti.style.left = `${offsetX}px`;
+      confetti.style.top = `${offsetY}px`;
+      const randomX = (Math.random() - 0.5) * 200;
+      const randomY = (Math.random() - 0.5) * 200;
+      confetti.style.transform = `translate(${randomX}px, ${randomY}px) scale(2)`;
+      //  setTimeout(() => {
+      //    confetti.style.transform = `translate(${randomX}px, ${randomY}px) scale(2)`;
+      //    confetti.style.opacity = "0";
+      //  }, i * 100);
+
+      confettiContainer.appendChild(confetti);
+
+      setTimeout(() => {
+        confetti.remove();
+      }, 2000);
+    }
+  };
+
   return (
     <div className="mood">
       <h1 className="mood-title">Who are you today?</h1>
       <div className="mood-emoji">
         {emojis.map((emoji, index) => (
-          <img key={index} src={emoji.img} alt={emoji.name} className="emoji" />
+          <div className="emoji-container" key={index}>
+            <img
+              key={index}
+              src={emoji.img}
+              alt={emoji.name}
+              className="emoji"
+              onClick={(e) => handleEmojiClick(emoji, e.target)}
+            />
+            <div className="overlay">
+              <p>{emoji.name}</p>
+              <div className="arrow"></div>
+            </div>
+          </div>
         ))}
       </div>
+      <div className="confetti-container"></div>
+      {/* {selectedEmoji && (
+        <div className="selected-mood">
+          <h2>Вы выбрали: {selectedEmoji.name}</h2>
+        </div>
+      )} */}
     </div>
   );
 };
